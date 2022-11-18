cf22e5a877e8793b2d301944fd82df844327d4e9
humanbeing <egeatmaca98@gmail.com>
2022-11-18 20:36:49 +0100

    scorecards added

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-    $data = [
+    $bar_line_data = [
         'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         'datasets' => [
             [
@@ -18,8 +18,23 @@
             ]
         ]
     ];
+    $bar_line_data_string = str_replace(' ', '%20%', json_encode($bar_line_data));
 
-    $data_string = str_replace(' ', '%20%', json_encode($data));
+    $doughnut_data = [
+        'labels' => ['A', 'B', 'C'],
+        'datasets' => [
+            [
+                'data' => [1, 2, 3],
+                'borderColor' => ['rgb(75, 192, 192)', 'rgb(237, 217, 76)', 'rgb(192, 75, 75)'],
+                'backgroundColor' => ['rgb(75, 192, 192)', 'rgb(237, 217, 76)', 'rgb(192, 75, 75)'],
+                'fill' => false
+            ]
+        ]
+    ];
+    $doughnut_data_string = str_replace(' ', '%20%', json_encode($doughnut_data));
+
+    $metric_number = 267;
+    $metric_percentage = 72;
 
     echo "
         <html>
@@ -31,21 +46,26 @@
 
             <body>
                 <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js'></script>
+                <h1> Your Dashboard </h1>
+                <div class='scorecard-container'>
+                    <span class='scorecard' id='myScoreCard'>Number: $metric_number </span>
+                    <span class='scorecard' id='myScoreCard1'>Percentage: {$metric_percentage}% </span>
+                </div>
                 <div class='wrapper'>
                     <div>
                         <h3>Line Chart</h3>
                         <canvas id='myChart' width='500' height='300'></canvas>
-                        <script src='./static/js/line_chart.js' canvas_id='myChart' title='Line Chart' data=$data_string></script>
+                        <script src='./static/js/line_chart.js' canvas_id='myChart' title='Line Chart' data=$bar_line_data_string></script>
                     </div>
                     <div>
                         <h3>Bar Chart</h3>
                         <canvas id='myChart1' width='500' height='300'></canvas>
-                        <script src='./static/js/bar_chart.js' canvas_id='myChart1' title='Bar Chart' data=$data_string></script>
+                        <script src='./static/js/bar_chart.js' canvas_id='myChart1' title='Bar Chart' data=$bar_line_data_string></script>
                     </div>
                     <div>
                         <h3>Doughnut Chart</h3>
                         <canvas id='myChart2' width='500' height='300'></canvas>
-                        <script src='./static/js/doughnut_chart.js' canvas_id='myChart2' title='Doughnut Chart' data=$data_string></script>
+                        <script src='./static/js/doughnut_chart.js' canvas_id='myChart2' title='Doughnut Chart' data=$doughnut_data_string></script>
                     </div>
                 </div>
             </body>
